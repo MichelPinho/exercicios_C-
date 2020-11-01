4fc9d4eded1ccd285a764a01edfbec75a6c9b0e3
Michel Pinho <jologpinho@gmail.com>
2020-11-01 10:17:59 -0300

    Informando se p número é maior, menor ou igual
    Exercício realizado na aula de C++, Universidade Estácio de Sá.

---
+++ Sem Título1.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+
+{
+
+float numA, numB;
+
+cout<<("Forneça o número A:")<<endl;
+
+cin>> numA;
+
+cout<<("Forneça o número B:")<<endl;
+
+cin>>numB;
+
+	if(numA < numB)
+		cout<<("O número B é maior");
+
+		if (numB < numA)
+			cout<<("Os número A é maior")<<endl;
+
+		else (numA == numB)
+			cout<<("Os números são iguais")<<endl;
+
+
+
+return(0);
+
+}
+
+
