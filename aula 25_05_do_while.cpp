978efa99f838a3180775eee036ebbdb7daa02b55
Michel Pinho <jologpinho@gmail.com>
2020-11-01 10:01:45 -0300

    Script de exercício com Do While
    Exercício realizado na aula de C++, Universidade Estácio de Sá.

---
+++ aula 25_05_do_while.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	int opcao;
+	
+	do
+	{
+		cout<<"MENU"<<endl;
+		cout<<"1 - INCLUIR "<<endl;
+		cout<<"2 - EXCLUIR "<<endl;
+		cout<<"3 - CONSULTAR "<<endl;
+		cout<<"9 - SAIR "<<endl;
+		cin>>opcao;
+		
+	switch (opcao){
+		
+		case 1: cout<<"VAI INCLUIR"<<endl;
+		break;
+		
+		case 2: cout<<"VAI EXCLUIR"<<endl;
+		break;
+		
+		case 3: cout<<"VAI CONSULTAR"<<endl;
+		break;
+		
+		case 9: cout<<"VAI SAIR"<<endl;
+		break;
+		
+		default: cout<<"OPCAO INVALIDA"<<endl;
+		
+	}
+	}
+	
+	while (opcao!=9);
+
+ system ("pause");
+return 0;
+
+}
