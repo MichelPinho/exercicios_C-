144aa2000e6045c3522ca54c143436f763e076fc
Michel Pinho <jologpinho@gmail.com>
2020-11-01 10:13:37 -0300

    Calculando IMC
    Exercício realizado na aula de C++, Universidade Estácio de Sá.

---
+++ main_exerc.5_IMC.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ 
+int main(int argc,char** argv) {
+	
+	float altura,imc;
+	string sexo;
+	
+	
+	cout<<"Entre com a sua altura: "<<endl;
+	cin>>altura;
+	
+	cout<<"Digite seu sexo (Masculino ou Feminino) : "<<endl;
+	cin>>sexo;
+	
+	
+	
+		
+			
+	
+
+   
+	system ("pause");
+	return 0;
+
+
+}
