74edcc71eb0a16d09a02e6dd3be57ae25d65ff28
Michel Pinho <jologpinho@gmail.com>
2020-11-01 09:58:02 -0300

    Script de exercício
    Exercício realizado na aula de C++, Universidade Estácio de Sá.

--- main.cpp
+++ main.cpp
@@ -2,36 +2,52 @@
 #include <cstdlib>
 using namespace std;
 
-int main ( )
-{
-    int matricula; 
-    float salario;
-    char indice;
+
+
+int main() {
+	
+	
+    int matricula[100], i=0, j=0; 
+    float salario[100], sal=0;
+    char indice[100];
+    
     do {
      cout <<"Matricula: ";
-     cin >> matricula;
-     if (matricula > 0){
+     cin >> matricula[i];
+     
+     j= matricula[i];
+     
+     if (matricula[i] > 0){
        cout << "Salario: R$ ";
-       cin >> salario;
+       cin >> salario[i];
        cout <<"Indice : ";
-       cin >> indice;
-       		switch (indice){
-          		case 'e': salario = salario * 1.4;
+       cin >> indice[i];
+       i++;
+    }
+    
+  
+    
+ } while ( j<<0 && i<100);
+ 
+		for (j=0; j <i; j++) {
+ 		cout << "Matricula: "<< matricula[j] << endl ;
+            switch (indice[j]){
+            	case 'e': sal = salario[j] * 1.4;
 				break; 
-           		case 'b': salario = salario * 1.2;
-				break;
-           		case 'r': salario = salario *1.05;
+				
+             	case 'b': sal = salario[j] * 1.2;
 				break;
 				
-           		default: cout << "Indice Invalido "<<endl;
-      }
-      	cout << "Matricula: "<< matricula ;     
-	cout  <<" Salario: R$ " << salario<<",00" << endl; 
+             	case 'r': sal = salario[j] * 1.05;
+				break;
+				
+             	default: cout << "Indice Invalido"<<endl;
+         }
+         	cout  <<" Salario: R$ "  <<sal << endl; 
     }
-  } while (matricula> 0);
-  
-  
-  system ("pause");
-  return 0;
-}
 
+
+system ("pause");
+return 0;
+
+}
